3da8c9e609ef2d3e7b1a101d9463dd4ed2bfb0bd
sofiaanderberg <sofiaanderberg@gmail.com>
2020-02-13 14:22:53 +0100

    tentant

--- ProgramKonstruktion/Tenant.cs
+++ ProgramKonstruktion/Tenant.cs
@@ -8,5 +8,32 @@ namespace ProgramKonstruktion
 {
     class Tenant
     {
+
+        private string ssn;
+        private string name;
+        private string phoneNumber;
+        private string emailAddress;
+        private DateTime rentingDate;
+
+        public string Ssn {
+            get => ssn;
+            set => ssn = value;
+        }
+        public string Name {
+            get => name;
+            set => name = value;
+        }
+        public string PhoneNumber {
+            get => phoneNumber;
+            set => phoneNumber = value;
+        }
+        public string EmailAddress {
+            get => emailAddress;
+            set => emailAddress = value;
+        }
+        public DateTime RentingDate {
+            get => rentingDate;
+            set => rentingDate = value;
+        }
     }
 }
