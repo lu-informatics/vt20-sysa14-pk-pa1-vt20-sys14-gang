a264f71e59f174b79583a70429c410a1194b5f95
sofiaanderberg <sofiaanderberg@gmail.com>
2020-02-17 16:07:42 +0100

    Revert ","
    This reverts commit 48238aba22b1a3b2e7fc85130898f49e55df15fb.

---
+++ ProgramKonstruktion/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgramKonstruktion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+   
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Button from view 1 has been clicked!!!");
+        }
+    }
+}
