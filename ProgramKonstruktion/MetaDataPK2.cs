cabd9c5616ac5ce12beb227dfa7dd084be93b443
Elinme <44189735+Elinme@users.noreply.github.com>
2020-02-18 14:18:51 +0100

    Create metadata class pk2

---
+++ ProgramKonstruktion/MetaDataPK2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramKonstruktion
+{
+    class MetaDataPK2
+    {
+        private static readonly string _host = "uwdb18.srv.lu.se";
+        private static readonly string _instanceName = "icssql001";
+        private static readonly string _dbName = "SYSA14_PK_ProgAssignment2";
+        private static readonly string _username = "sysa14reader";
+        private static readonly string _password = "INFreader1";
+    }
+}
