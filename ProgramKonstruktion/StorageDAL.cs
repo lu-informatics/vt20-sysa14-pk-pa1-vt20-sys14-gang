b210c6ae783146191b32a3d31c3db89970c5a09a
lovisawp <44405257+lovisawp@users.noreply.github.com>
2020-02-18 10:52:30 +0100

    Update StorageDAL
    Added connector and started on method for CreateStorage

--- ProgramKonstruktion/StorageDAL.cs
+++ ProgramKonstruktion/StorageDAL.cs
@@ -8,5 +8,22 @@ namespace ProgramKonstruktion
 {
     class StorageDAL
     {
+        private Connector c = new Connector();
+        private SqlConnection SQLConnection;
+
+        public StorageDAL() //constructor
+        {
+            c = new Connector();
+            SQLConnection = c.connection;
+        }
+        public Storage CreateStorage { 
+            
+            string queryCreateStorage = "INSERT INTO (@storageNbr, @price, @size, @address)"; 
+            SQLcommand command = new SQLcommand(queryCreateStorage, SQLConnection); 
+            
+            command.parameters.Add("@storageNbr", SQLDbType.NInt32).Value=storage.storageNbr; 
+
+        }
+}
     }
 }
