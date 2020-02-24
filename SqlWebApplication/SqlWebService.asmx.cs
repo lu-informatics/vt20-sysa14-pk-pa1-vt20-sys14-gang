ef3ebce23a515f5e5cae53b61ca7fd937bd327e8
Elinme <44189735+Elinme@users.noreply.github.com>
2020-02-24 11:19:22 +0100

    Created webmethod for sql

--- SqlWebApplication/SqlWebService.asmx.cs
+++ SqlWebApplication/SqlWebService.asmx.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Services;
+using ProgramKonstruktion; 
 
 namespace SqlWebApplication
 {
@@ -15,12 +16,42 @@ namespace SqlWebApplication
     // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
     // [System.Web.Script.Services.ScriptService]
     public class SqlWebService : System.Web.Services.WebService
+
+
     {
 
+        Connector c = new Connector(); 
+        StorageDAL storageDAL = new StorageDAL();
+        TenantDAL tenantDAL = new TenantDAL(); 
+
         [WebMethod]
-        public string HelloWorld()
+        public Tenant UpdateTenant(string ssn, string name, string phoneNbr, string email)
         {
-            return "Hello World";
+            return tenantDAL.UpdateTenant(ssn, name, phoneNbr, email); 
+
+
         }
+       
+
+
+        [WebMethod]
+        public bool CreateTenant(string ssn, string name, string phoneNbr, string email, string storageNbr, DateTime rentDate, string storageAddrress)
+        {
+            Tenant t = new Tenant();
+            t.Name = name;
+            t.Ssn = ssn;
+            t.PhoneNbr = phoneNbr;
+            t.Email = email;
+            t.StorageNbr = storageNbr;
+            t.RentDate = rentDate;
+            t.StorageAddress = storageAddrress; 
+            return tenantDAL.CreateTenant(t);
+
+            
+
+
+        }
+       
+
     }
 }
