7501e952a9d16e50c73b04d490b918c4acd28716
lovisawp <44405257+lovisawp@users.noreply.github.com>
2020-02-29 16:35:58 +0100

    Update GUI ws2
    Changed from shell to JFrame

--- WSJava1/src/se/lu/ics/grupp2/GUI_WS2.java
+++ WSJava1/src/se/lu/ics/grupp2/GUI_WS2.java
@@ -1,48 +1,93 @@
 package se.lu.ics.grupp2;
 
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Shell;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextArea;
+import java.awt.FlowLayout;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.JComboBox;
 
 public class GUI_WS2 {
 
-	protected Shell shell;
-
+	private JFrame frame;
+	SqlWebServiceSoapProxy myProxy = new SqlWebServiceSoapProxy(); 
+	
 	/**
 	 * Launch the application.
-	 * @param args
 	 */
 	public static void main(String[] args) {
-		try {
-			GUI_WS2 window = new GUI_WS2();
-			window.open();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	/**
-	 * Open the window.
-	 */
-	public void open() {
-		Display display = Display.getDefault();
-		createContents();
-		shell.open();
-		shell.layout();
-		while (!shell.isDisposed()) {
-			if (!display.readAndDispatch()) {
-				display.sleep();
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUI_WS2 window = new GUI_WS2();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
-		}
+		});
 	}
 
 	/**
-	 * Create contents of the window.
+	 * Create the application.
 	 */
-	protected void createContents() {
-		shell = new Shell();
-		shell.setSize(450, 300);
-		shell.setText("SWT Application");
-
+	public GUI_WS2() {
+		initialize();
 	}
 
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnViewFile = new JButton("View");
+		btnViewFile.setBounds(119, 26, 89, 23);
+		frame.getContentPane().add(btnViewFile);
+		
+		final JTextPane txtFileOutput = new JTextPane();
+		txtFileOutput.setBounds(10, 60, 197, 190);
+		frame.getContentPane().add(txtFileOutput);
+		
+		final JComboBox<String> cBoxFiles = new JComboBox<String>();
+		//Lista med valbara filer att visa
+		ArrayList<String>files = new ArrayList<String>(); 
+		files.add("Test"); 
+		files.add("Top Secret"); 
+		for (String value: files) { 
+			 cBoxFiles.addItem(value); 
+		}
+		 
+		frame.getContentPane().add(cBoxFiles);
+		cBoxFiles.setBounds(10, 26, 111, 23); 
+		// method for viewing a chosen file 
+		/*btnViewFile.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+						String chosenFile = cBoxFiles.getSelectedItem().toString(); 
+						//String fileOutput = myProxy.showFile(chosenFile); 
+						; 
+						//txtFileOutput.setText(fileOutput);
+					} catch (RemoteException e1) {
+						e1.printStackTrace();
+					} 
+				
+				
+				
+				
+			}
+		});*/
+		
+		
+	}
 }
