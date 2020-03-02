ad2f09387cb0def67ad600043aacb6a7dc30f6bd
lovisawp <44405257+lovisawp@users.noreply.github.com>
2020-03-02 17:18:45 +0100

    Updated GUI
    Made GUI bigger and changed for loop in btnView so that it workds

--- WSJava1/src/View/GUI_WS2.java
+++ WSJava1/src/View/GUI_WS2.java
@@ -17,6 +17,7 @@ import javax.swing.JTextPane;
 
 import se.lu.ics.grupp2.SqlWebServiceSoapProxy;
 import se.lu.ics.grupp2.Storage;
+import se.lu.ics.grupp2.Tenant;
 
 import javax.swing.JComboBox;
 
@@ -55,18 +56,18 @@ public class GUI_WS2 {
 	 */
 	private void initialize() throws RemoteException {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(200, 200, 700, 700);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		//Button for viewing file
 		JButton btnViewFile = new JButton("View");
-		btnViewFile.setBounds(289, 26, 89, 23);
+		btnViewFile.setBounds(492, 11, 89, 23);
 		frame.getContentPane().add(btnViewFile);
 		
 		//Textpane that sets text output from objects
 		final JTextPane txtOutput = new JTextPane();
-		txtOutput.setBounds(10, 60, 361, 190);
+		txtOutput.setBounds(20, 45, 561, 409);
 		frame.getContentPane().add(txtOutput);
 		
 		//Combobox that holds options
@@ -86,12 +87,9 @@ public class GUI_WS2 {
 		for (String value: files) { 
 		cBoxFiles.addItem(value); 
 		}
-		
-		
-		
-		
+				
 		frame.getContentPane().add(cBoxFiles);
-		cBoxFiles.setBounds(10, 26, 281, 23); 
+		cBoxFiles.setBounds(20, 11, 142, 23); 
 		
 		// method for viewing a chosen file 
 		btnViewFile.addActionListener(new ActionListener() {
@@ -99,17 +97,20 @@ public class GUI_WS2 {
 				 
 				txtOutput.setText(""); 
 				String chosenList = cBoxFiles.getSelectedItem().toString(); 
+				String storageOutput = "";
+				String tenantOutput= ""; 
 				
 				if (chosenList.contentEquals("Storage")){ 
 					try {
-						//Storage[] storages = myProxy.getListOfStorages(); 
-							for (Storage storage: myProxy.getListOfStorages()) {
-								txtOutput.setText(
+							Storage [] storageList = myProxy.getListOfStorages(); 
+							for (Storage storage : storageList) {
+								storageOutput+=(
 									"Storage number: " + storage.getNbr() + "\n"
 									+ "Price: " + storage.getPrice() + "\n"
-									+ "Size: " + storage.getSize() + "\n"
-									+ "Adress: " + storage.getAddress());
+									+ "Size: " + storage.getSize() + "\n" 
+									+ "Adress: " + storage.getAddress()+ "\n" + "\n" ); 
 							}
+							txtOutput.setText(storageOutput);
 					} catch (RemoteException e1) {
 						e1.printStackTrace();
 						txtOutput.setText("Failed to get list of storages");
@@ -117,6 +118,21 @@ public class GUI_WS2 {
 					
 				}
 				if (chosenList.contentEquals("Tenant")) {
+					try {
+						Tenant [] tenantList = myProxy.getTenantBookings(); 
+						for (Tenant tenant: myProxy.getTenantBookings()) {
+								tenantOutput+= (
+									"Email: " + tenant.getEmail() + "\n"
+									+ "Name: " + tenant.getName() + "\n"
+									+ "Phone number: " + tenant.getPhoneNbr() + "\n"
+									+ "Ssn: " + tenant.getSsn() + "\n" 
+									+ "Storage number: " + tenant.getEmail()+ "\n" + "\n" );		
+							txtOutput.setText(tenantOutput); 	
+						}
+					} catch (RemoteException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
 				}
 			}
 		});
