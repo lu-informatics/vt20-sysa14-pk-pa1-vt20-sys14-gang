65f860e6dfc4e29e201f7540bc2ca68d41259563
amandalowgren <44254332+amandalowgren@users.noreply.github.com>
2020-03-04 19:32:42 +0100

    Made an new Console App

---
+++ ConsoleAppFile/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hej");
+            WebServiceWebERP.WebServiceERP12SoapClient o = new WebServiceWebERP.WebServiceERP12SoapClient();
+            //  RefService.WebServiceAssignmentSoapClient obj = new RefService.WebServiceAssignmentSoapClient();
+            Console.WriteLine("Pls fill in a fileName:");
+            string s = Console.ReadLine();
+            Console.WriteLine(o.ShowFile(s));
+            Console.ReadLine();
+
+
+        }
+    }
+}
